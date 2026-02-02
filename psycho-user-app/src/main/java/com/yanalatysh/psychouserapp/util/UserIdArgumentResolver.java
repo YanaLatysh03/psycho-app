1669bd094ffa57a0a89c1f97b23e49574b713a91
yanalatysh <yanalatysh1@gmail.com>
2026-02-03 00:38:28 +0300

    Add user service and tracker service to gateway

--- psycho-user-app/src/main/java/com/yanalatysh/psychouserapp/util/UserIdArgumentResolver.java
+++ psycho-user-app/src/main/java/com/yanalatysh/psychouserapp/util/UserIdArgumentResolver.java
@@ -4,11 +4,13 @@ import org.jspecify.annotations.Nullable;
 import org.springframework.core.MethodParameter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Component
 public class UserIdArgumentResolver implements HandlerMethodArgumentResolver {
     // Проверяет наличие аннотации и тип Long.
     @Override
